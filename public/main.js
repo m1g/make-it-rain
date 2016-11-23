4698c32af1f74f5cc0cef54cccd6de68a3a2abb2
m1g <miguelmalcolm@outlook.com>
2016-11-23 09:32:53 -0500

    added non-working javascript code

--- public/main.js
+++ public/main.js
@@ -1,6 +1,30 @@
+const qs = s => document.querySelector(s)
+const drop = qs('.drop')
+
+const rainButton = () => {
+  const buttonAction = qs('.rainAction button')
+  buttonAction.setInterval(() => {
+    drop.style.top = '0px'
+  }, 1000)
+
+  rainButton()
+}
+
+const resetDrop = () => {
+  if (drop.style.top === 100) {
+    clearInterval()
+  } else {
+    rainButton()
+  }
+}
+
 const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+  for (let i = 0; i < drop.length; i++) {
+    const li = document.createElement('li')
+    li.textContent = drop[i].addEventListener(rainButton)
+  }
+  drop.addEventListener('click', rainButton)
+  drop.addEventListener(resetDrop)
 }
 
 document.addEventListener('DOMContentLoaded', main)
-

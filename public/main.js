0ce6816b5e7a8935df3baaa02e0a51c8f9fd2074
m1g <miguelmalcolm@outlook.com>
2016-11-25 10:17:17 -0500

    added create random drop feature

--- public/main.js
+++ public/main.js
@@ -5,29 +5,25 @@ const rainButton = () => {
   setInterval(() => {
     drop.style.top = '0px'
   }, 1000)
-
   const animate = () => {
     drop.style.top = `${parseInt(drop.style.top) + 1}px`
   }
-
-  setInterval(animate, 16.66)
+  setInterval(animate, 1)
 }
 
-// const resetDrop = () => {
-//   if (drop.style.top === 100) {
-//     clearInterval()
-//   } else {
-//     rainButton()
-//   }
-// }
+const randNumber = () => {
+  const randomize = Math.floor(Math.random() * 500)
+  drop.style.left = `${randomize}px`
+  console.log(randomize)
+}
 
 const main = () => {
-  // for (let i = 0; i < drop.length; i++) {
-  //   const li = document.createElement('li')
-  //   li.textContent = drop[i].addEventListener(rainButton)
-  // }
+  const rainDrops = qs('.rainBox button[name=rainAction]')
+  for (var i = 0; i < rainDrops.length; i++) { // Drops multiple rain drops
+    rainDrops[i].addEventListener('button', rainButton)
+  }
+  qs('.rainBox button[name=rainAction]').addEventListener('click', randNumber)
   qs('.rainBox button[name=rainAction]').addEventListener('click', rainButton)
-  // drop.addEventListener(resetDrop)
 }
 
 document.addEventListener('DOMContentLoaded', main)

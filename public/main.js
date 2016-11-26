fa665513f57ee7b1c50bf66f52b649f9b2230290
m1g <miguelmalcolm@outlook.com>
2016-11-25 21:05:14 -0500

    added calling random x-axis drops

--- public/main.js
+++ public/main.js
@@ -1,3 +1,15 @@
+/************
+OBJECTIVES
+************/
+
+// Didn't figure out document.body.clientWidth
+// Need rain to fall full height of rainBox
+// Rain should stop/disappear at bottom of rainBox
+// Didn't figure out document.body.clientHeight
+// Want to try different speeds for droplets
+// Know for loop and if/else statement should be incuded
+// But not sure how or where (╯°□°)╯︵ ┻━┻
+
 const qs = s => document.querySelector(s)
 
 const rainButton = () => {
@@ -9,32 +21,26 @@ const rainButton = () => {
 
   drop.appendChild(img)
   qs('.drop').appendChild(drop)
-  //
-  // const animate = () => {
-  //   drop.style.top = `${parseInt(drop.style.top) + 1}px`
-  // }
-  // setInterval(() => {
-  //   drop.style.top = '0px'
-  // }, 1000)
-  //
-  // setInterval(animate, 1)
-}
 
-// const randNumber = () => {
-//   const drop = document.createElement('div')
-//   drop.className = 'drop'
-//
-//   const randomize = Math.floor(Math.random() * 500)
-//   drop.style.left = `${randomize}px`
-//   console.log(randomize)
-// }
+  const animate = () => {
+    drop.style.top = `${parseInt(drop.style.top) + 1}px`
+  }
+  setInterval(() => {
+    drop.style.top = '0px'
+  }, 1000)
+
+  setInterval(animate, 1)
+
+  const randomize = Math.floor(Math.random() * 550)
+  drop.style.left = `${randomize}px`
+  console.log(randomize)
+}
 
 const main = () => {
   // const rainDrops = qs('.rainBox button[name=rainAction]')
   // for (var i = 0; i < rainDrops.length; i++) { // Drops multiple rain drops
   //   rainDrops[i].addEventListener('button', rainButton)
   // }
-  // qs('.rainBox button[name=rainAction]').addEventListener('click', randNumber)
   qs('.rainBox button[name=rainAction]').addEventListener('click', rainButton)
 }
 

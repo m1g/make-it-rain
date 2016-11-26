dfd42ea5b7754101d16389b5a62194cb7522e2fb
m1g <miguelmalcolm@outlook.com>
2016-11-25 19:50:08 -0500

    papas got some brand new code

--- public/main.js
+++ public/main.js
@@ -1,28 +1,40 @@
 const qs = s => document.querySelector(s)
-const drop = qs('.drop')
 
 const rainButton = () => {
-  setInterval(() => {
-    drop.style.top = '0px'
-  }, 1000)
-  const animate = () => {
-    drop.style.top = `${parseInt(drop.style.top) + 1}px`
-  }
-  setInterval(animate, 1)
+  const drop = document.createElement('div')
+  drop.className = 'drop'
+
+  const img = document.createElement('img')
+  img.src = 'droplet.svg'
+
+  drop.appendChild(img)
+  qs('.drop').appendChild(drop)
+  //
+  // const animate = () => {
+  //   drop.style.top = `${parseInt(drop.style.top) + 1}px`
+  // }
+  // setInterval(() => {
+  //   drop.style.top = '0px'
+  // }, 1000)
+  //
+  // setInterval(animate, 1)
 }
 
-const randNumber = () => {
-  const randomize = Math.floor(Math.random() * 500)
-  drop.style.left = `${randomize}px`
-  console.log(randomize)
-}
+// const randNumber = () => {
+//   const drop = document.createElement('div')
+//   drop.className = 'drop'
+//
+//   const randomize = Math.floor(Math.random() * 500)
+//   drop.style.left = `${randomize}px`
+//   console.log(randomize)
+// }
 
 const main = () => {
-  const rainDrops = qs('.rainBox button[name=rainAction]')
-  for (var i = 0; i < rainDrops.length; i++) { // Drops multiple rain drops
-    rainDrops[i].addEventListener('button', rainButton)
-  }
-  qs('.rainBox button[name=rainAction]').addEventListener('click', randNumber)
+  // const rainDrops = qs('.rainBox button[name=rainAction]')
+  // for (var i = 0; i < rainDrops.length; i++) { // Drops multiple rain drops
+  //   rainDrops[i].addEventListener('button', rainButton)
+  // }
+  // qs('.rainBox button[name=rainAction]').addEventListener('click', randNumber)
   qs('.rainBox button[name=rainAction]').addEventListener('click', rainButton)
 }
 
